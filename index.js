d264105986c06fd918cc0ea5f11a5276804e827a
vzaidman <bmokDy>
2016-07-05 11:09:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var Matrix = require('./Matrix')
+var PointTypes = require('./Point')
+
+module.exports = {
+  Point: PointTypes.Point,
+  LinkedPoint: PointTypes.LinkedPoint,
+  Matrix: Matrix
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# partial-paper
+
+A platform for using parts of [Paper.js](http://paperjs.org/).
